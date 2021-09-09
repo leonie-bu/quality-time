6fa925cbd33a31306c4e5512999e528cdf161d45
leonie-bu <leonie.buschbeck@gmail.com>
2021-09-09 22:18:53 +0200

    ✨ Create sizeMap and Typography function

--- src/app/components/Typography/Typography.tsx
+++ src/app/components/Typography/Typography.tsx
@@ -14,3 +14,33 @@ type TypographyProps = {
     | 'mm';
   children: ReactNode;
 };
+
+const sizeMap = {
+  xl: styles.xl,
+  l: styles.l,
+  m: styles.m,
+  sdarkcolor: styles.sdarkcolor,
+  slightcolor: styles.slightcolor,
+  xsregweight: styles.xsregweight,
+  xslightweight: styles.xslightweight,
+  mm: styles.mm,
+};
+
+export default function Typography({
+  size,
+  children,
+}: TypographyProps): JSX.Element {
+  switch (size) {
+    case 'xl':
+      return <h1 className={sizeMap[size]}>{children}</h1>;
+    case 'l':
+      return <h2 className={sizeMap[size]}>{children}</h2>;
+    case 'm':
+    case 'sdarkcolor':
+    case 'slightcolor':
+    case 'xsregweight':
+    case 'xslightweight':
+    case 'mm':
+      return <p className={sizeMap[size]}>{children}</p>;
+  }
+}
