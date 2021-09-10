8cb8300289a789d2b4abc2abfa2d6bb09994fe66
leonie-bu <leonie.buschbeck@gmail.com>
2021-09-10 17:11:23 +0200

    ✨ Create variable to display default React Calendar

---
+++ src/app/components/Calendar/Calendar.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactCalendar from './Calendar';
+
+export default {
+  title: 'Component/Calendar',
+  component: ReactCalendar,
+};
+
+export const Cal = (): JSX.Element => <ReactCalendar></ReactCalendar>;
