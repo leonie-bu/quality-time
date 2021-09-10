511887019f29a5d1d600b7c659b6cdbb06123098
leonie-bu <leonie.buschbeck@gmail.com>
2021-09-10 17:10:50 +0200

    ✨ Create basic function for ReactCalendar

---
+++ src/app/components/Calendar/Calendar.tsx
@@ -0,0 +1,22 @@
+/*import React, { useState } from 'react';
+import Calendar from 'react-calendar';
+
+export default function ReactCalendar(): JSX.Element {
+  const [date, setDate] = useState(new Date());
+  const onChange = (date) => {
+    setDate(date);
+  };
+} */
+
+import React, { useState } from 'react';
+import Calendar from 'react-calendar';
+
+export default function ReactCalendar(): JSX.Element {
+  const [value, onChange] = useState(new Date());
+
+  return (
+    <div>
+      <Calendar onChange={onChange} value={value} />
+    </div>
+  );
+}
