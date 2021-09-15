b7bf93f70e02c89d3df73b76a73f86bd2308dac3
leonie-bu <leonie.buschbeck@gmail.com>
2021-09-15 16:45:11 +0200

    ✨ Add Flower Icon

---
+++ src/app/components/assets/FlowerIconSmall.tsx
@@ -0,0 +1,89 @@
+import React from 'react';
+
+export default function FlowerIconSmall(
+  props: React.SVGProps<SVGSVGElement>
+): JSX.Element {
+  return (
+    <svg
+      width="126"
+      height="126"
+      viewBox="0 0 126 126"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M63.0001 79.3635C67.34 79.3635 71.5021 77.6395 74.5709 74.5707C77.6397 71.5019 79.3637 67.3398 79.3637 62.9999C79.3637 58.66 77.6397 54.4978 74.5709 51.429C71.5021 48.3602 67.34 46.6362 63.0001 46.6362C58.6601 46.6362 54.498 48.3602 51.4292 51.429C48.3604 54.4978 46.6364 58.66 46.6364 62.9999C46.6364 67.3398 48.3604 71.5019 51.4292 74.5707C54.498 77.6395 58.6601 79.3635 63.0001 79.3635V79.3635Z"
+        stroke="white"
+        stroke-width="5"
+        stroke-miterlimit="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M68.4547 46.6363C68.4547 46.6363 73.9092 35.7272 73.9092 24.8182C73.9092 13.9091 63.0001 3 63.0001 3C63.0001 3 52.091 13.9091 52.091 24.8182C52.091 35.7272 57.5456 46.6363 57.5456 46.6363"
+        stroke="white"
+        stroke-width="5"
+        stroke-miterlimit="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M46.6364 57.5455C46.6364 57.5455 35.7273 52.0909 24.8182 52.0909C13.9091 52.0909 3 63 3 63C3 63 13.9091 73.9091 24.8182 73.9091C35.7273 73.9091 46.6364 68.4545 46.6364 68.4545"
+        stroke="white"
+        stroke-width="5"
+        stroke-miterlimit="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M68.4547 79.3636C68.4547 79.3636 73.9092 90.2727 73.9092 101.182C73.9092 112.091 63.0001 123 63.0001 123C63.0001 123 52.091 112.091 52.091 101.182C52.091 90.2727 57.5456 79.3636 57.5456 79.3636"
+        stroke="white"
+        stroke-width="5"
+        stroke-miterlimit="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M79.3636 57.5455C79.3636 57.5455 90.2727 52.0909 101.182 52.0909C112.091 52.0909 123 63 123 63C123 63 112.091 73.9091 101.182 73.9091C90.2727 73.9091 79.3636 68.4545 79.3636 68.4545"
+        stroke="white"
+        stroke-width="5"
+        stroke-miterlimit="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M55.2875 47.5746C55.2875 47.5746 51.4312 36.0001 43.713 28.2874C36.0003 20.5747 20.5748 20.5747 20.5748 20.5747C20.5748 20.5747 20.5748 36.0001 28.2875 43.7183C36.0003 51.431 47.5748 55.2874 47.5748 55.2874"
+        stroke="white"
+        stroke-width="5"
+        stroke-miterlimit="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M47.5748 70.7124C47.5748 70.7124 36.0003 74.5688 28.2875 82.2869C20.5748 89.9997 20.5748 105.425 20.5748 105.425C20.5748 105.425 36.0003 105.425 43.713 97.7124C51.4312 89.9997 55.2875 78.4251 55.2875 78.4251"
+        stroke="white"
+        stroke-width="5"
+        stroke-miterlimit="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M78.4305 70.7124C78.4305 70.7124 89.9941 74.5688 97.7123 82.2869C105.425 89.9997 105.425 105.425 105.425 105.425C105.425 105.425 89.9996 105.425 82.2868 97.7124C74.5686 89.9997 70.7123 78.4251 70.7123 78.4251"
+        stroke="white"
+        stroke-width="5"
+        stroke-miterlimit="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M70.7123 47.5746C70.7123 47.5746 74.5686 36.0001 82.2868 28.2874C89.9996 20.5747 105.425 20.5747 105.425 20.5747C105.425 20.5747 105.425 36.0001 97.7123 43.7183C89.9996 51.431 78.425 55.2874 78.425 55.2874"
+        stroke="white"
+        stroke-width="5"
+        stroke-miterlimit="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}
