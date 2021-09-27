78e2ef6f4d31dcca5f6e44605c80a80524fd01bf
leonie-bu <leonie.buschbeck@gmail.com>
2021-09-27 14:12:43 +0200

    🔀 Trying to resolve merge conflict

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import styles from './App.module.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 function App(): JSX.Element {
   return (
