888818f14ba5662cb8b25ac0e52b74b845cefe5c
leonie-bu <leonie.buschbeck@gmail.com>
2021-09-27 14:08:30 +0200

    🔀 Trying to resolve merge conflict

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Start from './pages/StartPage/StartPage';
+import styles from './App.module.css';
 
 function App(): JSX.Element {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route path="/">
-          <Start />
-        </Route>
-      </Switch>
+      <div className={styles.App}>
+        <Switch>
+          <Route path="/about">
+            <main>About</main>
+          </Route>
+          <Route path="/">
+            <main>Home</main>
+          </Route>
+        </Switch>
+      </div>
     </BrowserRouter>
   );
 }
