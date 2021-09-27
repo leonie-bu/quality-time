03b47fc5a693f1ee279ebb9fa847b6e5fea8a1a9
leonie-bu <leonie.buschbeck@gmail.com>
2021-09-27 10:40:04 +0200

    🎨 Add the content elements to start page

--- src/app/pages/StartPage/StartPage.tsx
+++ src/app/pages/StartPage/StartPage.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import FlowerIconSmall from '../../components/assets/FlowerIconSmall';
+import LetsGoIcon from '../../components/assets/LetsGo';
 import Typography from '../../components/Typography/Typography';
 import styles from './StartPage.module.css';
 
 export default function Start(): JSX.Element {
   return (
     <main className={styles.container}>
-      <Typography size="l">Hello</Typography>
+      <div className={styles.content}>
+        <FlowerIconSmall />
+        <Typography size="l" className={styles.text}>
+          Nimm Dir Zeit für das, was oft zu kurz kommt:
+        </Typography>
+        <Typography size="l">
+          Zeit für Dich, <br /> Zeit für Gutes
+        </Typography>
+        <LetsGoIcon className={styles.letsgo} />
+      </div>
     </main>
   );
 }
