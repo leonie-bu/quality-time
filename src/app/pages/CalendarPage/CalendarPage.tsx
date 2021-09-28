81290657ac113b9fa5c921b321d67cc5338565f3
leonie-bu <leonie.buschbeck@gmail.com>
2021-09-28 17:58:30 +0200

    🎨 Add components to Calendar Page

--- src/app/pages/CalendarPage/CalendarPage.tsx
+++ src/app/pages/CalendarPage/CalendarPage.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import styles from './CalendarPage.module.css';
 import PlusBackgroundIcon from '../../components/assets/PlusBackgroundIcon';
 import ReactCalendar from '../../components/Calendar/Calendar';
+import Textarea from '../../components/Textarea/Textarea';
+import Navigation from '../../components/Navigation/Navigation';
 
 export default function Calendar(): JSX.Element {
   return (
@@ -10,8 +12,10 @@ export default function Calendar(): JSX.Element {
         <ReactCalendar />
       </section>
       <section className={styles.textarea}>
+        <Textarea />
         <PlusBackgroundIcon />
       </section>
+      <Navigation className={styles.navigation} activeLink="calendar" />
     </main>
   );
 }
