87fdb655f2f5253b1efe32ca86e4e13942db9e67
leonie-bu <leonie.buschbeck@gmail.com>
2021-09-28 11:16:37 +0200

    🎨 Remove not needed components

--- src/app/pages/CalendarPage/CalendarPage.tsx
+++ src/app/pages/CalendarPage/CalendarPage.tsx
@@ -1,19 +1,17 @@
 import React from 'react';
 import styles from './CalendarPage.module.css';
 import CalendarDate from '../../components/CalendarDate/CalendarDate';
-import Textarea from '../../components/Textarea/Textarea';
 import PlusBackgroundIcon from '../../components/assets/PlusBackgroundIcon';
-import Navigation from '../../components/Navigation/Navigation';
 
 export default function Calendar(): JSX.Element {
   return (
     <main className={styles.container}>
-      <div className={styles.content}>
+      <section className={styles.calendar}>
         <CalendarDate />
-        <Textarea />
+      </section>
+      <section className={styles.textarea}>
         <PlusBackgroundIcon />
-        <Navigation />
-      </div>
+      </section>
     </main>
   );
 }
