a86ecf4a09033ae088bb6a082569bb0313c1ade7
leonie-bu <leonie.buschbeck@gmail.com>
2021-09-28 12:13:43 +0200

    🎨 Use onClick and date in ReactCalendar

--- src/app/components/Calendar/Calendar.tsx
+++ src/app/components/Calendar/Calendar.tsx
@@ -12,18 +12,20 @@ import React, { useState } from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import './CustomCalendar.css';
+import Typography from '../Typography/Typography';
 
 export default function ReactCalendar(): JSX.Element {
-  const [value, onChange] = useState(new Date());
+  const [date, setDate] = useState(new Date());
 
   return (
     <div>
       <Calendar
-        onChange={onChange}
-        value={value}
+        onClickDay={setDate}
+        value={date}
         next2Label={null}
         prev2Label={null}
       />
+      <Typography size="m"> {date.toDateString()}</Typography>
     </div>
   );
 }
