bf55ca3dc2cec8730d41a6341d7e8e9ef247dd35
leonie-bu <leonie.buschbeck@gmail.com>
2021-09-29 22:01:07 +0200

    🎨 Add useStates here and adapt props of components

--- src/app/pages/CalendarPage/CalendarPage.tsx
+++ src/app/pages/CalendarPage/CalendarPage.tsx
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './CalendarPage.module.css';
-import PlusBackgroundIcon from '../../components/assets/PlusBackgroundIcon';
 import ReactCalendar from '../../components/Calendar/Calendar';
 import Textarea from '../../components/Textarea/Textarea';
 import Navigation from '../../components/Navigation/Navigation';
 
 export default function Calendar(): JSX.Element {
+  const [date, setDate] = useState(new Date());
+  const [activity, setActivity] = useState('');
+
+  function handleSubmit() {
+    console.log('Hey');
+  }
+
   return (
     <main className={styles.container}>
       <section className={styles.calendar}>
-        <ReactCalendar />
+        <ReactCalendar setDate={setDate} date={date} />
       </section>
       <section className={styles.textarea}>
-        <Textarea />
-        <PlusBackgroundIcon />
+        <Textarea
+          value={activity}
+          onChange={(event) => setActivity(event.target.value)}
+          handleSubmit={() => handleSubmit()}
+        />
       </section>
       <Navigation className={styles.navigation} activeLink="calendar" />
     </main>
