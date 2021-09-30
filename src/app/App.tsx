4161d5c4bd5c5ca1cefc396f98f073dcccd2f5f4
leonie-bu <leonie.buschbeck@gmail.com>
2021-09-30 22:14:08 +0200

    ✨ Add route for overview page

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Calendar from './pages/CalendarPage/CalendarPage';
+import Overview from './pages/OverviewPage/OverviewPage';
 import Start from './pages/StartPage/StartPage';
 
 function App(): JSX.Element {
@@ -9,6 +10,9 @@ function App(): JSX.Element {
       <Switch>
         <Route path="/calendar">
           <Calendar />
+        </Route>
+        <Route path="/overview">
+          <Overview />
         </Route>
         <Route path="/">
           <Start />
