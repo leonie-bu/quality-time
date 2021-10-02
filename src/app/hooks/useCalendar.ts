b6a57c22a5f987be529520df89972d1fff92bcf6
leonie-bu <leonie.buschbeck@gmail.com>
2021-10-02 21:51:07 +0200

    🎨 Write function in hook for done activity

--- src/app/hooks/useCalendar.ts
+++ src/app/hooks/useCalendar.ts
@@ -5,6 +5,7 @@ export default function useActivityCard(): {
   activityCards: ActivityCards[];
   addActivityCard: (activityCard: ActivityCards) => void;
   removeActivityCard: (newActivityCard: ActivityCards) => void;
+  doneActivityCard: (newActivityCard: ActivityCards) => void;
 } {
   const [activityCards, setActivityCards] = useLocalStorage<ActivityCards[]>(
     'activityCards',
@@ -21,5 +22,18 @@ export default function useActivityCard(): {
     );
   }
 
-  return { activityCards, addActivityCard, removeActivityCard };
+  function doneActivityCard(newActivityCard: ActivityCards) {
+    setActivityCards([
+      ...activityCards.filter(
+        (activityCard) => activityCard.date !== newActivityCard.date
+      ),
+      newActivityCard,
+    ]);
+  }
+  return {
+    activityCards,
+    addActivityCard,
+    removeActivityCard,
+    doneActivityCard,
+  };
 }
