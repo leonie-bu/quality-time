9dd02fda3fd833fd7db25a36ad9db9d8646c3653
leonie-bu <leonie.buschbeck@gmail.com>
2021-10-02 21:53:11 +0200

    🎨 Write func to change cards (edit) and delete card

--- src/app/pages/OverviewPage/OverviewPage.tsx
+++ src/app/pages/OverviewPage/OverviewPage.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ActiveActivity from '../../components/ActiveActivity/ActiveActivity';
+import DoneActivity from '../../components/DoneActivity/DoneActivity';
 import Navigation from '../../components/Navigation/Navigation';
 import Typography from '../../components/Typography/Typography';
 import useActivityCard from '../../hooks/useCalendar';
@@ -7,11 +8,8 @@ import type { ActivityCards } from '../../types';
 import styles from './OverviewPage.module.css';
 
 export default function Overview(): JSX.Element {
-  const { activityCards, removeActivityCard } = useActivityCard();
-
-  function handleonClickDone() {
-    console.log('done');
-  }
+  const { activityCards, removeActivityCard, doneActivityCard } =
+    useActivityCard();
 
   function handleonClickEdit() {
     console.log('edit');
@@ -21,21 +19,42 @@ export default function Overview(): JSX.Element {
     removeActivityCard(activityCard);
   }
 
+  function handleonClickDone(activityCard: ActivityCards) {
+    doneActivityCard({ ...activityCard, status: 'done' });
+  }
+
   return (
     <main className={styles.container}>
       <Typography size="l">Übersicht</Typography>
       <section className={styles.activity}>
-        {activityCards.map((activityCard) => (
-          <ActiveActivity
-            date={activityCard.date}
-            onClickDone={() => handleonClickDone}
-            onClickEdit={() => handleonClickEdit}
-            onClickDelete={() => handleonClickDelete(activityCard)}
-            handleSubmit={() => handleonClickDelete}
-            onChange={() => handleonClickDelete}
-            value={activityCard.activity}
-          />
-        ))}
+        {activityCards.map((activityCard) => {
+          switch (activityCard.status) {
+            case 'active':
+            case undefined:
+              return (
+                <ActiveActivity
+                  key={activityCard.date}
+                  type="active"
+                  date={activityCard.date}
+                  onClickDone={() => handleonClickDone(activityCard)}
+                  onClickEdit={() => handleonClickEdit}
+                  onClickDelete={() => handleonClickDelete(activityCard)}
+                  handleSubmit={() => console.log('Test')}
+                  onChange={() => console.log('Test')}
+                  value={activityCard.activity}
+                />
+              );
+            case 'done':
+              return (
+                <DoneActivity
+                  key={activityCard.date}
+                  date={activityCard.date}
+                  type="done"
+                  onClickDelete={() => handleonClickDelete(activityCard)}
+                />
+              );
+          }
+        })}
       </section>
       <Navigation activeLink="overview" />
     </main>
